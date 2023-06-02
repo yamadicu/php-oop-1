f95488437a23564833c3a5abe878507d36ad72b8
yamadicu <dicutia@gmail.com>
2023-06-02 11:05:02 +0200

    fine progetto

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
 <?php 
+include_once __DIR__ . '/partials/head.php'
+include_once __DIR__ . '/partials/footer.php'
+
 class movie{
-public $attore; 
-function __construct($eta_attore){
-    $this->attore = $eta_attore;
+public $titolo;
+public $durata;
+public $poster;
+public function __construct($_titolo, $_duration, $_poster){
+    $this->titolo = $_titolo;
+    $this->duration = $_duration;
+    $this->poster = $_poster;
 }
+
+public function get_movie_deails(){
+    return "movie: $this->titolo "
 }
+
+}
+
+
 
 $Genere = new movie(20);
 
